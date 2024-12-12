677088a67391325a780bd9afa601cd406f78249f
kyungho <epsoe@naver.com>
2024-12-12 11:45:48 +0900

    조건문 주사위 문제 수정

--- dice.js
+++ dice.js
@@ -4,9 +4,17 @@ function dice(input) {
   if (a === b && b === c) {
     return 10000 + a * 1000;
   }
-  if (a === b || b === c || a === c) {
+  // 두 개의 주사위만 같은 경우
+  if (a === b) {
+    return 1000 + a * 100;
+  }
+  if (b === c) {
     return 1000 + b * 100;
   }
+  if (a === c) {
+    return 1000 + a * 100;
+  }
+
   return Math.max(a, b, c) * 100;
 }
 
