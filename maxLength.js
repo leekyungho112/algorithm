81ced5db16dde73aebd4c333d7fbcdc425880520
kyungho <epsoe@naver.com>
2024-12-13 10:30:55 +0900

    최대값 문제 풀이(reduce 활용)

--- maxLength.js
+++ maxLength.js
@@ -1,15 +1,20 @@
 function maxLength(input) {
   const num = input.map(Number);
 
-  let max = 0;
-  let index = 0;
+  //   let max = 0;
+  //   let index = 0;
 
-  for (let i = 0; i < num.length; i++) {
-    if (max < num[i]) {
-      max = num[i];
-      index = i + 1;
-    }
-  }
+  //   for (let i = 0; i < num.length; i++) {
+  //     if (max < num[i]) {
+  //       max = num[i];
+  //       index = i + 1;
+  //     }
+  //   }
+  //   return { max, index };
+
+  const max = num.reduce((acc, cur) => Math.max(acc, cur), num[0]);
+  const index = num.indexOf(max) + 1;
+
   return { max, index };
 }
 
