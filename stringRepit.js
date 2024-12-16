29ca697cdaff208dd7913e983aee647c4e205604
kyungho <epsoe@naver.com>
2024-12-16 14:54:48 +0900

    다른풀이 방법 2675

--- stringRepit.js
+++ stringRepit.js
@@ -1,14 +1,27 @@
 function stringRepit(input) {
-  const testCase = input[0];
+  //   const testCase = input[0];
 
-  for (let t = 1; t <= testCase; t++) {
-    const string = input[t].split(' ');
+  //   for (let t = 1; t <= testCase; t++) {
+  //     const string = input[t].split(' ');
+  //     let answer = '';
+
+  //     for (let x of string[1]) {
+  //       for (let i = 0; i < string[0]; i++) {
+  //         answer += x;
+  //       }
+  //     }
+  //     console.log(answer);
+  //   }
+
+  // 다른 풀이
+
+  const testCase = Number(input[0]);
+
+  for (let i = 1; i <= testCase; i++) {
+    const [r, s] = input[i].split(' ');
     let answer = '';
-
-    for (let x of string[1]) {
-      for (let i = 0; i < string[0]; i++) {
-        answer += x;
-      }
+    for (let j = 0; j <= s.length; j++) {
+      answer += s.charAt(j).repeat(r);
     }
     console.log(answer);
   }
