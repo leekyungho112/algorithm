fc2dce698bae63d5590be7a6ea2f71c606ec3c9f
leekyungho <epsoe@naver.com>
2025-01-13 16:44:02 +0900

    17609

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let fs = require("fs");
 // let averageOne = require('./average_1');
 // let stringAdd = require('./stringAdd');
 // let stringRepit = require('./stringRepit');
-let solution = require("./19939");
+let solution = require("./17609");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
 const result = solution(input);
