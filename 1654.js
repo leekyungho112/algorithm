89119571ea641461e66bad219ef5975948537515
leekyungho <epsoe@naver.com>
2025-01-15 14:05:27 +0900

    1654

--- 1654.js
+++ 1654.js
@@ -2,24 +2,22 @@ function solution(input) {
   const [n, k] = input[0].split(" ").map(Number);
   const arr = input.slice(1, n + 1).map(Number);
 
-  let start = 1;
+  let start = 0;
   let end = Math.max(...arr);
   let result = 0;
 
   while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-
+    let mid = parseInt((start + end) / 2);
     let total = 0;
-
     for (let x of arr) {
       total += parseInt(x / mid);
     }
 
-    if (total >= k) {
+    if (total < k) {
+      end = mid - 1;
+    } else {
       result = mid;
       start = mid + 1;
-    } else {
-      end = mid - 1;
     }
   }
   console.log(result);
