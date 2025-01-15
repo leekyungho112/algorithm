b2ce97b74906881ae6bc0b60d560ac1d6200f61a
leekyungho <epsoe@naver.com>
2025-01-15 13:52:48 +0900

    1654

---
+++ 1654.js
@@ -0,0 +1,28 @@
+function solution(input) {
+  const [n, k] = input[0].split(" ").map(Number);
+  const arr = input.slice(1, n + 1).map(Number);
+
+  let start = 1;
+  let end = Math.max(...arr);
+  let result = 0;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    let total = 0;
+
+    for (let x of arr) {
+      total += parseInt(x / mid);
+    }
+
+    if (total >= k) {
+      result = mid;
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  console.log(result);
+}
+
+module.exports = solution;
