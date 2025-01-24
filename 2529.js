cba4fa94bdfab51ceacc25b19cd3c056c0c59bf0
leekyungho <epsoe@naver.com>
2025-01-24 17:06:42 +0900

    2529

--- 2529.js
+++ 2529.js
@@ -1,6 +1,51 @@
 function solution(input) {
   const n = Number(input[0]);
-  const k = input[1].split("");
+  const signs = input[1].split(" ");
 
-  console.log(k);
+  let selected = [];
+  let visited = Array(10).fill(false);
+
+  let maxValue = "";
+  let minValue = "";
+  function isValid(prev, next, sign) {
+    // 부등호 조건 검사
+    if (sign === "<") return prev < next;
+    if (sign === ">") return prev > next;
+    return false;
+  }
+
+  function dfs(depth) {
+    if (depth === n + 1) {
+      const join = selected.join("");
+
+      if (join > maxValue) {
+        maxValue = join;
+      }
+      if (!minValue || join < minValue) {
+        minValue = join;
+      }
+      return;
+    }
+
+    for (let i = 0; i <= 9; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      if (depth > 0 && !isValid(selected[depth - 1], i, signs[depth - 1])) {
+        continue;
+      }
+
+      selected.push(i);
+
+      visited[i] = true;
+      dfs(depth + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  }
+
+  dfs(0);
+  console.log(maxValue);
+  console.log(minValue);
 }
+module.exports = solution;
