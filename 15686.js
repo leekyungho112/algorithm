0ebe70fe3aed063f2dea561307780fe7214ed77b
leekyungho <epsoe@naver.com>
2025-02-04 11:17:52 +0900

    4803

---
+++ 15686.js
@@ -0,0 +1,51 @@
+function solution(input) {
+  let [n, m] = input[0].split(" ").map(Number);
+  let chicken = [];
+  let house = [];
+  let answer = 1e9;
+  for (let i = 1; i <= n; i++) {
+    let line = input[i].split(" ").map(Number);
+    for (let j = 0; j < n; j++) {
+      if (line[j] === 1) house.push([i, j]);
+      if (line[j] === 2) chicken.push([i, j]);
+    }
+  }
+
+  let visited = new Array(chicken.length).fill(false);
+  let selected = [];
+
+  function dfs(depth, start) {
+    if (depth === m) {
+      let result = [];
+      for (let i of selected) {
+        result.push(chicken[i]);
+      }
+      let sum = 0;
+      for (let [hx, hy] of house) {
+        let temp = 1e9;
+        for (let [cx, cy] of result) {
+          temp = Math.min(temp, Math.abs(hx - cx) + Math.abs(hy - cy));
+        }
+        sum += temp;
+      }
+      answer = Math.min(answer, sum);
+      return;
+    }
+
+    for (let i = start; i < chicken.length; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      selected.push(i);
+      visited[i] = true;
+      dfs(depth + 1, i + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  }
+
+  dfs(0, 0);
+  console.log(answer);
+}
+
+module.exports = solution;
