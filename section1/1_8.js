4a658c275286b54beb90a613600b4f6097893e6c
leekyungho <epsoe@naver.com>
2025-02-09 16:35:51 +0900

    1_8

--- section1/1_8.js
+++ section1/1_8.js
@@ -1,16 +1,22 @@
 function solution(input) {
-  const num = input[0].split(" ").map(Number);
+  const num = input.map(Number);
+
   let sum = num.reduce((acc, cur) => acc + cur, 0);
   let answer = [];
+  let flag = 0;
   for (let i = 0; i < num.length; i++) {
     for (let j = i + 1; j < num.length; j++) {
-      //   console.log(num[i], num[j]);
       if (sum - (num[i] + num[j]) === 100) {
-        answer = [...num.slice(0, 5), ...num.slice(7)];
+        answer = num.filter((_, index) => index !== i && index !== j);
+        flag = 1;
+        break;
       }
     }
+    if (flag === 1) {
+      break;
+    }
   }
-  console.log(answer);
+  console.log(answer.sort((a, b) => a - b).join("\n"));
 }
 
 module.exports = solution;
